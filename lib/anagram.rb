5652ce3cbb3a258996e9d955005f49145ae3b7a9
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-07 00:33:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,16 @@
 # Your code goes here!
 require 'pry'
 class Anagram
+  attr_accessor :word
   def initialize(word)
     @word = word
   end
   
   def match(anagrams)
-    matches = []
     letters = @word.split("")
+    anagrams.select do |element|
+      letters.sort == element.split("").sort
+    end
     binding.pry
   end
 end
